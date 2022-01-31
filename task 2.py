b42687c8d2ec1ec80933b9ed2cf5dbdd1b5f0a6c
litauev <84537406+litauev@users.noreply.github.com>
2022-01-31 18:15:01 +0300

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,10 @@
+name = input('enter name')
+    surname = input('enter surname')
+    year = int(input('enter year'))
+    city = input('enter city')
+    email = input('enter email')
+    telephone = input('input telephone')
+'''
+def my_func (name, surname, year, city, email, telephone):
+     return ' '.join([name, surname, year, city, email, telephone])
+print(my_func(surname = 'Qwerty', name = 'Qwerty', year = '2022', city = 'New York', email = 'qwerty@mail.ru', telephone = '123'))
